07c89db1bd5f1cfc13a4b94facc7f751b4750bbb
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-05-08 10:45:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 console.log("main.js is running");
 window.onload = function () {
-  var buttonChange = document.getElementById("btn");
-  var hexColour = document.getElementById("res-hex");
-  var rgbColour = document.getElementById("res-rgb");
+  let main = document.querySelector("main#main");
+  let button = document.querySelector("#btn");
+  let hex_input = document.querySelector("#res-hex input");
+  let rgb_input = document.querySelector("#res-rgb");
+  let r_input = document.querySelector("#res-rgb input[name=red-input]");
+  let g_input = document.querySelector("#res-rgb input[name=green-input]");
+  let b_input = document.querySelector("#res-rgb input[name=blue-input]");
 
-  buttonChange.onclick = function () {
+  button.onclick = function () {
     console.log("clicked");
     function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     function rgbToHex(r, g, b) {
-      return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+      return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
     var r = getRandomInt(0, 255);
     var g = getRandomInt(0, 255);
@@ -21,22 +25,20 @@ window.onload = function () {
     console.log("Blue : " + b);
     console.log("Hex : " + rgbToHex(r, g, b));
 
-    document.getElementById("main").style.background =
-      "rgb(" + r + "," + g + "," + b + ")";
-    document.getElementById("main").style.transition = ".7s";
-    hexColour.innerHTML = rgbToHex(r, g, b);
-    rgbColour.innerHTML = r + "|" + g + "|" + b;
+    main.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    main.style.transition = ".7s";
+    hex_input.value = rgbToHex(r, g, b);
+    r_input.value = r;
+    g_input.value = g;
+    b_input.value = b;
     document.getElementById("res").style.transition = "1s";
 
     if (r + g + b <= 255) {
-      document.getElementById("btn").style.color = "#fff";
+      document.getElementById("btn").style.color = "#fffA";
       document.getElementById("res").style.color = "#fff";
     } else {
       document.getElementById("btn").style.color = "#000";
       document.getElementById("res").style.color = "#000";
-    }
-    if (r > 50 && g > 50 && b > 50) {
-      document.querySelectorAll(".res").style.background = "#000";
     }
   };
 
